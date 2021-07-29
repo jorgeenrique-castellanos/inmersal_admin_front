5cbb399d1768a231ba8b0ad4b264d63c8f156cb5
jorge castellanos <jorgeenrique.castellanos@lopublicaste.com>
2021-07-29 11:41:57 -0500

    creada function para envio de mensajes

--- src/components/23_view_logout_user/form_create/FormCreate.js
+++ src/components/23_view_logout_user/form_create/FormCreate.js
@@ -43,21 +43,6 @@ export default function FormCreate() {
     enviarAlServidor(loginExitoso, loginErrado, config);
   };
 
-  function showMessage(message, error = true) {
-    const config = {
-      position: "top-right",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined
-    };
-    return error
-      ? toast.error(message, config)
-      : toast.success(message, config);
-  }
-
   return (
     <>
       <ToastContainer />
