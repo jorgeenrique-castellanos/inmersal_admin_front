cb89eafe8ba61c5a1795ddf5d6eae1b00dcba815
jorge castellanos <jorgeenrique.castellanos@lopublicaste.com>
2021-08-09 13:03:16 -0500

    Servidor final

--- src/components/24_component_countrys/form_create/form_create_params.js
+++ src/components/24_component_countrys/form_create/form_create_params.js
@@ -6,20 +6,15 @@ export default user => {
 
   form_params["create_server"] = {
     method: "POST",
-    url: "http://127.0.0.1:8000/api/pais"
+    url: "https://inmersal-back.lopublicaste.co/public/api/pais"
   };
   
 
   form_params["validation_rules"] = {  
     id: yup.string().required(),
     country: yup.string().required(),
-<<<<<<< HEAD
-    alpha2: yup.string().required(),
-    alpha3: yup.string().required(),
-=======
-    country_alpha_2: yup.string().max(2, "Maximo dos caracteres").required(),
-    country_alpha_3: yup.string().max(3, "Maximo tres caracteres").required(),
->>>>>>> e945b1890b5c63241b6815e0bec39291613ad1de
+    alpha2: yup.string().max(2, "Maximo dos caracteres").required(),
+    alpha3: yup.string().max(3, "Maximo tres caracteres").required(),
   };
 
   // form_params["parent_id"] = {
